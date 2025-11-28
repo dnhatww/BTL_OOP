dfecfa96a1a2e2b1d21a94bd086b3d9709399aae
huongskc <huong2308skc@gmail.com>
2025-11-28 23:14:25 +0700

    update repository

--- tailieuptit-main/src/main/java/com/tailieuptit/demo/repository/CommentRepository.java
+++ tailieuptit-main/src/main/java/com/tailieuptit/demo/repository/CommentRepository.java
@@ -7,13 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-    /**
-     * [LIÊN KẾT VỚI DOCUMENTSERVICE]
-     * Hàm này được DocumentServiceImpl (hàm mapToDetailDTO) gọi
-     * để lấy các comment GỐC (không phải reply) của một tài liệu.
-     */
-    List<Comment> findByDocumentIdAndParentIdIsNullOrderByCreatedAtDesc(Long documentId);
-
     Page<Comment> findByUserId(Long userId, Pageable pageable);
 }
--- tailieuptit-main/src/main/java/com/tailieuptit/demo/repository/RoleRepository.java
+++ tailieuptit-main/src/main/java/com/tailieuptit/demo/repository/RoleRepository.java
@@ -5,11 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-    /**
-     * [LIÊN KẾT VỚI USERSERVICE]
-     * Được gọi bởi registerUser() để gán vai trò mặc định (ROLE_USER)
-     * cho người dùng mới.
-     */
     Optional<Role> findByName(String name);
 }
